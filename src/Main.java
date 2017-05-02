dac04be7aca04eb598d835bb6db382b873f3769b
Patrick Ebert <p.ebert16@web.de>
2017-05-02 22:50:37 +0200

    First commit
    This is the very first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by pebert on 5/2/2017.
+ */
+// I M P O R T S
+public class Main {
+    // M A I N  M E T H O D E
+    public static void main(String [] args)
+    {
+        System.out.println("This is the beginning...");
+    }
+}
